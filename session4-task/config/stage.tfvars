5485b4cd48d3cb50a44b99fe58ecee9a7d2c0352
Nurbek <nurimankg@yahoo.com>
2021-06-04 00:38:20 +0000

    u

---
+++ session4-task/config/stage.tfvars
@@ -0,0 +1,3 @@
+env = "stage"
+instance_type = "t2.micro"
+cidr    ["10.0.0.0/16"]
---
+++ session4-task/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_version = "~> 0.15.0"
+  required_providers {          
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.40.0"
+    }
+  }
+}
